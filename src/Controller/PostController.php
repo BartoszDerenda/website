8bd9c17cc64689ab8e67d719b6c84d3d6ffd0229
BartoszDerenda <bartoszderenda00@gmail.com>
2022-07-31 15:37:10 +0200

    Day 3
    Creating + connecting database (website).
    Changes in Twig, adding Bootstrap.
    Manager Registry troubleshooting.

---
+++ src/Controller/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @Route("/post", name="post.")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     */
+    public function create(Request $request, ManagerRegistry $doctrine) {
+        $post = new Post();
+
+        $post->setTitle('This is a title');
+
+        $em = $doctrine->getManager();
+    }
+}
