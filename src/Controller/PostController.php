94932142ccfcc548438421a10c947b6153befeaf
BartoszDerenda <bartoszderenda00@gmail.com>
2022-08-02 14:58:40 +0200

    Day 4
    Views, create, delete - basic CRUD

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,23 +17,67 @@ class PostController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts
         ]);
     }
 
     /**
      * @Route("/create", name="create")
      * @param Request $request
+     * @param ManagerRegistry $doctrine
      */
-    public function create(Request $request, ManagerRegistry $doctrine) {
+    public function create(Request $request, ManagerRegistry $doctrine)
+    {
+        // create new post with a title
         $post = new Post();
-
         $post->setTitle('This is a title');
 
+        // entity manager
         $em = $doctrine->getManager();
+        $em->persist($post);                        // https://stackoverflow.com/questions/1069992/jpa-entitymanager-why-use-persist-over-merge
+        $em->flush();
+        // remember to call flush() after a bunch of querries are ready to be sent
+
+        return $this->redirect($this->generateUrl('post.index'));
     }
+
+    /**
+     * @Route ("/show/{id}", name="show")
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post)
+    {
+
+        // create the view
+        return $this->render('post/show.html.twig', [
+            'post' =>$post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param Post $post
+     * @param ManagerRegistry $doctrine
+     */
+    public function remove(Post $post, ManagerRegistry $doctrine)
+    {
+        $em = $doctrine->getManager();
+
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'Post was removed.');
+
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+
 }
